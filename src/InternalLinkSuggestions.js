98704ee53ead806463657aabe9ed6bf634fdc384
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/InternalLinkSuggestions.js
+++ src/InternalLinkSuggestions.js
@@ -2,7 +2,6 @@
  * Insert internal links based on the articles from other wikis
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/InternalLinkSuggestions.js]] ([[File:User:Helder.wiki/Tools/InternalLinkSuggestions.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, laxbreak: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
